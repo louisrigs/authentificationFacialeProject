8e6a5c14942518fe06a94f1c31469eb4e845f0d7
Louis <louisrigaux@hotmail.com>
2020-11-04 12:03:49 +0100

    Mise en place de la database avec notre gallery et les probes

--- venv/main.py
+++ venv/main.py
@@ -6,19 +6,19 @@ import matplotlib.image as mpimg
 
 n = 2
 maxprobes = 100
-
+gallery = []
+probes = []
+probes1 = []
+probes2 = []
+probes1name = []
+probes2name = []
 
 def dataLoad():
 	# path = "C:/Users/benoît/Documents/IMT Lille Douai/M1/ODATA/Projet/ODATA_project_authentication_data/data/dataset1"
 	path = "D:/louis/Documents/Institut Mines-Télécom Lille-Douai 2017-2023/4-M1 - 2020-2021/Modules/P2 - ODATA - Outils pour la Data Science/Projet Authentification faciale/data/dataset1/images"
 
-	gallery = []
-	probes = []
-	probes1 = []
-	probes2 = []
-
-	compteur = 0
 	currentName = ''
+	compteur = 0
 
 	print('Selection des fichiers .jpg...\n', end='')
 
@@ -26,7 +26,6 @@ def dataLoad():
 		current_directory_path = os.path.abspath(root)
 
 		for f in files:
-
 			nameVersion, ext = os.path.splitext(f)
 			if ext == ".jpg":
 				current_image_path = os.path.join(current_directory_path, f)
@@ -35,23 +34,21 @@ def dataLoad():
 
 				if len(probes2) < maxprobes:
 					probes2.append(current_image)
-					print(name)
+					probes2name.append(nameVersion)
 				else:
 					if len(probes1) < maxprobes:
 						if name == currentName:
-							if compteur <= n:
+							if compteur < n:
 								probes1.append(current_image)
+								probes1name.append(nameVersion)
+								compteur += 1
 							else:
 								gallery.append(current_image)
-							compteur += 1
 						else:
-							if compteur <= n:
-								probes1.append(current_image)
-							else:
-								gallery.append(current_image)
 							compteur = 1
+							probes1.append(current_image)
+							probes1name.append(nameVersion)
 							currentName = name
-
 					else:
 						gallery.append(current_image)
 
@@ -64,6 +61,8 @@ def dataLoad():
 	print(len(probes2))
 	print(len(gallery))
 
+	print(probes1name)
+
 	iter = 0
 	for image in probes1:
 		if iter <= 10:
@@ -72,22 +71,5 @@ def dataLoad():
 			plt.imshow(image)
 			plt.show()
 		iter += 1
-
-	"""
-	print('Affichage des images ...',end='')
-	
-	iter = 0
-	for image in gallery:
-		iter += 1
-		plt.figure()
-		plt.axis("off")
-		plt.imshow(image)
-		plt.show()
-		if iter == 100:
-			break
-
-	print('100%\n',end='')
-"""
-
 
 dataLoad()
